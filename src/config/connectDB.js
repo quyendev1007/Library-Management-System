aedfc055f802b816b0dadf3895db2bcab12bb58b
QuyenDz <qh10072504@gmail.com>
2025-06-05 22:14:01 +0700

    feat: update MongoDB connection logic to use dynamic URI construction

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,14 +2,23 @@ import mongoose from "mongoose";
 
 let isConnected = false;
 
+// do pass có chứa ký tự đặc biệt
+
 const connectDB = async () => {
+  const password = encodeURIComponent(process.env.PASS_MONGO);
+  const mongoUri = `${
+    process.env.MONGODB_URI_BEFORE + password + process.env.MONGODB_URI_AFTER
+  }`;
+
   if (isConnected) {
     console.log("Đã kết nối MongoDB trước đó.");
     return;
   }
 
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    console.log(mongoUri);
+
+    await mongoose.connect(mongoUri);
 
     isConnected = true;
     console.log("Kết nối MongoDB thành công.");
