e1a1a25624a6cd7d89ce2061d650fad94ac0274c
QuyenDz <qh10072504@gmail.com>
2025-06-07 09:01:45 +0700

    feat(auth): implement logout and refresh token functionality, update routes and middleware

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,8 +1,9 @@
 import bcrypt from "bcryptjs";
 import { User } from "../models/user";
 import { pickUser } from "../utils/formatters";
-import { generateToken } from "../providers/jwtProvider";
+import { generateToken, verifyToken } from "../providers/jwtProvider";
 import { StatusCodes } from "http-status-codes";
+import ms from "ms";
 
 export const register = async (req, res) => {
   try {
@@ -63,18 +64,88 @@ export const login = async (req, res) => {
 
     const refreshToken = await generateToken(
       userInfo,
-      process.env.ACCESS_TOKEN_SECRET_SIGNATURE,
-      process.env.ACCESS_TOKEN_LIFE
+      process.env.REFRESH_TOKEN_SECRET_SIGNATURE,
+      process.env.REFRESH_TOKEN_LIFE
     );
 
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: true,
+      maxAge: ms("14 days"),
+      sameSite: "none",
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      maxAge: ms("14 days"),
+      sameSite: "none",
+    });
+
     return res.status(StatusCodes.OK).json({
-      ...pickUser(user),
+      user: pickUser(user),
       refreshToken,
       accessToken,
     });
   } catch (error) {
     return res.status(400).json({
       error: "Lỗi khi đăng nhập",
+      message: error.message,
+    });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
+
+    return res.status(StatusCodes.OK).json({
+      message: "Đăng xuất thành công",
+    });
+  } catch (error) {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      error: "Lỗi khi đăng xuất",
+      message: error.message,
+    });
+  }
+};
+
+export const refreshToken = async (req, res) => {
+  try {
+    const clientRefreshToken = req.cookies?.refreshToken;
+
+    if (!clientRefreshToken) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        error: "Refresh token het han hoac khong ton tai",
+      });
+    }
+
+    const isValid = await verifyToken(
+      clientRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET_SIGNATURE
+    );
+
+    if (!isValid) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        error: "Refresh token khong hop le",
+      });
+    }
+
+    const userInfo = isValid;
+
+    const newAccessToken = await generateToken(
+      userInfo,
+      process.env.ACCESS_TOKEN_SECRET_SIGNATURE,
+      process.env.ACCESS_TOKEN_LIFE
+    );
+
+    return res.status(StatusCodes.OK).json({
+      accessToken: newAccessToken,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      error: "Lỗi khi làm mới token",
       message: error.message,
     });
   }
